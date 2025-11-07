473fc1d11f4cb130a5c6ff70a9e772f152e8fe82
Alexander Ramos <ramos.olivera.alexander@gmail.com>
2025-11-07 13:13:58 -0300

    Cambia paletas de colores en themes y color. Aplica logo a launcher y pantalla login

--- app/src/main/java/com/example/app1/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/app1/ui/screens/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.app1.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -22,11 +24,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.app1.R
 import com.example.app1.Routes
 import com.example.app1.data.AuthRepository
 import kotlinx.coroutines.launch
@@ -47,10 +56,28 @@ fun LoginScreen(navController: NavController){
         horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
     )
     {
+        // Logo
+        Image(
+            painter = painterResource(id = R.drawable.logo_pastillero),
+            contentDescription = "Logo de la app",
+            modifier = Modifier
+                .size(120.dp)
+                .padding(bottom = 20.dp)
+        )
+
         Text(
-            text = "Bienvenido",
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(bottom = 32.dp)
+            text = buildAnnotatedString {
+                append("Pastillero\n")
+
+                withStyle(style = SpanStyle(
+                    color = Color.Blue)
+                ){
+                    append("Inteligente IoT")
+                }
+            },
+            style = MaterialTheme.typography.headlineLarge.copy(
+                textAlign = TextAlign.Center),
+            modifier = Modifier.fillMaxWidth().padding(bottom = 32.dp)
         )
 
         // campo usuario
